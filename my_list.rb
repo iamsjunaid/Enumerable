5b0c76fa47f56d86c0214aaf301705732fb8be78
Martin Karugaba <martinkarugaba21@gmail.com>
2023-07-27 11:18:20 +0300

    Add simple test cases

--- my_list.rb
+++ my_list.rb
@@ -11,3 +11,17 @@ class MyList
     @list.each { |item| yield item }
   end
 end
+
+# Create list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
+
+# Test #filter
+puts(list.filter(&:even?)) # [2, 4]
